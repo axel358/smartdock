401bd3a8d00af708e545f3549a90fb3be55b3205
Axel358 <edgar.serrano.9803@gmail.com>
2022-08-21 11:21:02 -0400

    Make SecondaryLauncher extend Launcher activity

--- app/src/main/java/cu/axel/smartdock/activities/SecondaryLauncherActivity.java
+++ app/src/main/java/cu/axel/smartdock/activities/SecondaryLauncherActivity.java
@@ -1,16 +1,3 @@
 package cu.axel.smartdock.activities;
 
-import android.app.Activity;
-import android.os.Bundle;
-import cu.axel.smartdock.R;
-import android.widget.Toast;
-
-public class SecondaryLauncherActivity extends Activity
-{
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_secondarylauncheractivity);
-    }
-}
+public class SecondaryLauncherActivity extends LauncherActivity {}
