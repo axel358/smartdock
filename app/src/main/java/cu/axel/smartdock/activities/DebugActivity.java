25f299605a7786c531a9c2e3b706cc974efafdb7
Axel358 <edgar.serrano.9803@gmail.com>
2023-03-27 19:04:09 +0000

    fix: Make DebugActivity appcompat

--- app/src/main/java/cu/axel/smartdock/activities/DebugActivity.java
+++ app/src/main/java/cu/axel/smartdock/activities/DebugActivity.java
@@ -1,22 +1,22 @@
 package cu.axel.smartdock.activities;
 
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
 import cu.axel.smartdock.R;
 import cu.axel.smartdock.utils.Utils;
 
-public class DebugActivity extends Activity {
+public class DebugActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this, R.style.DialogTheme);
+        MaterialAlertDialogBuilder dialog = new MaterialAlertDialogBuilder(this);
 		dialog.setTitle(R.string.something_wrong);
 		final String report = getIntent().getStringExtra("report");
         dialog.setMessage(report);
