6fd39be3364fbcf954fb06bc4053e4ef4f634fe5
Axel358 <edgar.serrano.9803@gmail.com>
2023-07-15 07:42:39 -0400

    fix: Remove unused PreferencesFragment.kt

--- app/src/main/java/cu/axel/smartdock/fragments/PreferencesFragment.kt
+++
@@ -1,8 +0,0 @@
-package cu.axel.smartdock.fragments
-import androidx.preference.PreferenceFragmentCompat;
-
-class PreferencesFragment: PreferenceFragmentCompat() {
-	override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences_main, rootKey)
-    }
-}
