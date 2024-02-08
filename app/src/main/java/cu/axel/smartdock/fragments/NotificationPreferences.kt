2ddcf4a9dbc0c90a8f067cac87468ed5fe27295c
Axel358 <edgar.serrano.9803@gmail.com>
2024-02-08 14:04:43 -0500

    fix(Notification display time): Input type

--- app/src/main/java/cu/axel/smartdock/fragments/NotificationPreferences.kt
+++ app/src/main/java/cu/axel/smartdock/fragments/NotificationPreferences.kt
@@ -1,11 +1,20 @@
 package cu.axel.smartdock.fragments
 
 import android.os.Bundle
+import android.text.InputType
+import android.view.inputmethod.EditorInfo
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 import cu.axel.smartdock.R
 
 class NotificationPreferences : PreferenceFragmentCompat() {
     override fun onCreatePreferences(arg0: Bundle?, arg1: String?) {
         setPreferencesFromResource(R.xml.preferences_notification, arg1)
+
+        val notificationTimeout: EditTextPreference? = findPreference("notification_timeout")
+        notificationTimeout?.setOnBindEditTextListener { editText ->
+            editText.inputType = InputType.TYPE_CLASS_NUMBER
+            editText.imeOptions = EditorInfo.IME_ACTION_GO
+        }
     }
 }
